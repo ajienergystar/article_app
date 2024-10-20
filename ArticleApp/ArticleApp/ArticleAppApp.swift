9f15216e6a241b0d06727fd66590a8138b02147a
macAji <ajienergystar@gmail.com>
2024-10-20 19:09:08 +0800

    Create First Project Files

---
+++ ArticleApp/ArticleApp/ArticleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleAppApp.swift
+//  ArticleApp
+//
+//  Created by mac on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ArticleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
