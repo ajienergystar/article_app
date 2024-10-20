517ce6447e49b3ed2d68f63a35db05de430a72aa
macAji <ajienergystar@gmail.com>
2024-10-20 20:14:42 +0800

    Create First Login Page and InputFieldView Component

--- ArticleApp/ArticleApp/ContentView.swift
+++ ArticleApp/ArticleApp/ContentView.swift
@@ -10,10 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Article App")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .padding(.bottom, 42)
+            Button(action: {}) {
+                Text("Sign In")
+                    .fontWeight(.heavy)
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(LinearGradient(gradient: Gradient(colors: [.pink, .purple]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(40)
+            }
+            
+            HStack {
+                Spacer()                          // spacer to push text to the right
+                Text("Forgotten Password?")
+                    .fontWeight(.thin)            // make the font thinner
+                    .foregroundColor(Color.blue)  // make the color blue
+                    .underline()                  // underline the text
+            }.padding(.top, 16)
         }
         .padding()
     }
